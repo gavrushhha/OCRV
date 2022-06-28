2834bc4825f38225731f5693b0b74c6c4ee68b50
GitAlexandr <sashabushuev03@mail.ru>
2022-06-28 16:23:45 +0300

    change module struct, fix dependencies

--- src/ticketinfo/ocrv/urls.py
+++ src/ticketinfo/ocrv/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from app import views
+from ticketinfo.app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     # path('', include('app.urls')),
 ]
