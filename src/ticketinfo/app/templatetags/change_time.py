9f5bcfed5db0a57401a3de6676eca57a19d2050c
m.angelina <m.angellina.am@gmail.com>
2022-07-05 16:59:56 +0300

    change utils and add null_replace

--- src/ticketinfo/app/templatetags/change_time.py
+++
@@ -1,8 +0,0 @@
-from datetime import timedelta
-from django import template
-
-register = template.Library()
-
-@register.filter(name='change_timedelta')
-def change_timedelta(value: int) -> str:
-    return str(timedelta(seconds=value))
